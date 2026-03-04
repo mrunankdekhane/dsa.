25f88a8e77f5da0d754a404e0663ad6474a73b37
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-03-04 17:20:19 +0530

    dsa file

---
+++ matrix.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int count = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(mat[i][j] == 1) {
+                    bool special = true;
+
+                    for(int k = 0; k < n; k++) {
+                        if(k != j && mat[i][k] == 1) {
+                            special = false;
+                            break;
+                        }
+                    }
+
+                    for(int k = 0; k < m && special; k++) {
+                        if(k != i && mat[k][j] == 1) {
+                            special = false;
+                            break;
+                        }
+                    }
+
+                    if(special) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
